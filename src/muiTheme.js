dd3283c86afda694377993bd128ff96d4c091244
Ryan <rdturner40@gmail.com>
2021-01-01 05:47:33 -0500

    slight changes

--- src/muiTheme.js
+++ src/muiTheme.js
@@ -15,11 +15,13 @@ export const theme = createMuiTheme({
 });
 
 theme.typography.h2 = {
+  fontSize: "4rem",
   "@media (max-width:600px)": {
     fontSize: "3rem",
   },
 };
 theme.typography.h3 = {
+  fontSize: "3rem",
   "@media (max-width:600px)": {
     fontSize: "2rem",
   },
