a2b91de88495ddf900b77d6dded9bd329a0f4c1c
Ryan <rdturner40@gmail.com>
2021-04-21 14:06:49 -0400

    added resume for school

---
+++ src/components/Resume/ResumeStyle.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+export const useStyles = makeStyles({
+  title: {
+    color: "#808080",
+    marginBottom: ".5em",
+  },
+  text: {
+    color: "black",
+    textAlign: "center",
+  },
+});
